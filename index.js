710fb5a369e8de76098a5a847b64a3e991e48c4c
lillamanda <110566581+lillamanda@users.noreply.github.com>
2024-02-29 21:37:08 +0100

    further testing

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 
 
+var strLocation = window.location.href;
+var strHash = window.location.hash;
 
+console.log(strLocation);
+console.log(strHash);
+
+strLocation = "http://127.0.0.1:3000/index.html/whatever";
+console.log(strLocation)
 
 
